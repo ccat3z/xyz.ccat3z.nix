f8ae78188e494521acde79868081e5fa5bd1084e
ccat3z <fzhang.chn@outlook.com>
2024-01-09 01:33:52 +0000

    Fix vscode remote server

--- modules/default.nix
+++ modules/default.nix
@@ -18,4 +18,7 @@
   # GPG
   programs.gnupg.agent.enable = true;
   programs.gnupg.agent.enableSSHSupport = true;
+
+  # Support unpatched dynamic binaries like vscode remote server
+  programs.nix-ld.enable = true;
 }
