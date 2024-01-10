aa15c3d8ab0248f6ea2839e8d092d2c011f7230f
ccat3z <fzhang.chn@outlook.com>
2024-01-10 11:35:37 +0000

    Refactor flake.nix and hosts

--- flake.nix
+++ flake.nix
@@ -6,53 +6,68 @@
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, sops-nix, ... }@inputs: {
-    nixosConfigurationsBase = {
+  outputs = { self, nixpkgs, sops-nix, ... }@inputs:
+    let
       system = "x86_64-linux";
-      specialArgs = {
-        inherit sops-nix;
-      };
-      modules = [
-        ./modules
-        ./secrets
-        ({ nixpkgs.overlays = [ self.overlays.default ]; })
-      ];
-    };
-    nixosConfigurations = (
-      let
-        inherit (nixpkgs) lib;
-        inherit (builtins) listToAttrs attrNames readDir;
-        base = self.nixosConfigurationsBase;
-      in
-      listToAttrs (map
-        (x: {
-          name = lib.strings.removeSuffix ".nix" x;
-          value = nixpkgs.lib.nixosSystem (base // {
-            modules = base.modules ++ [ (./hosts + ("/" + x)) ];
-          });
-        })
-        (attrNames (readDir ./hosts)))
-    );
-
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
-    packages.x86_64-linux = import ./pkgs { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
-    overlays.default = import ./pkgs/overlay.nix;
-
-    devShells.x86_64-linux.default =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
-      in
-      pkgs.mkShell {
-        packages = with pkgs; [
-          sops
-          gnupg
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      nixosConfigurationsBase = {
+        inherit system;
+        specialArgs = {
+          inherit sops-nix;
+        };
+        modules = [
+          ./modules
+          ./secrets
+          ({ nixpkgs.overlays = [ self.overlays.default ]; })
         ];
-
-        shellHook = ''
-          echo -e '\033[34mIn dev shell\033[0m' >&2
-          exec $SHELL
-        '';
       };
-  };
+      nixosConfigurations = (
+        let
+          inherit (nixpkgs) lib;
+          inherit (builtins) listToAttrs attrNames readDir;
+          base = self.nixosConfigurationsBase;
+        in
+        listToAttrs (map
+          (x:
+            let
+              hostName = lib.strings.removeSuffix ".nix" x;
+            in
+            {
+              name = hostName;
+              value = lib.nixosSystem (base // {
+                modules = base.modules ++ [
+                  (./hosts + ("/" + x))
+                  {
+                    networking.hostName = hostName;
+                    networking.domain = "ccat3z.xyz";
+                  }
+                ];
+              });
+            })
+          (attrNames (readDir ./hosts)))
+      );
+
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      packages.${system} = import ./pkgs { inherit pkgs; };
+      overlays.default = import ./pkgs/overlay.nix;
+
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            sops
+            gnupg
+          ];
+
+          shellHook = ''
+            echo -e '\033[34mIn dev shell\033[0m' >&2
+            exec $SHELL
+          '';
+        };
+    };
 }
