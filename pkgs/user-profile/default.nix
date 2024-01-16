e54ab371d05a4d2a76eaa4f23f0560af773afdbe
ccat3z <fzhang.chn@outlook.com>
2024-01-16 11:03:09 +0800

    user-profile: Add nixos-rebuild

--- pkgs/user-profile/default.nix
+++ pkgs/user-profile/default.nix
@@ -1,8 +1,9 @@
-{ lib, buildEnv, nix, ... }:
+{ lib, buildEnv, nix, nixos-rebuild, ... }:
 buildEnv {
     name = "user-profile";
     paths = [
         nix
+        nixos-rebuild
     ];
     pathsToLink = ["/bin" "/etc" "/share"];
 }
