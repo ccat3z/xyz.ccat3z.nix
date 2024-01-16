7baac2778cc529a8b5ee1890148004ce7ef00549
ccat3z <fzhang.chn@outlook.com>
2024-01-16 10:33:50 +0800

    Add ssh authorized keys for root

--- modules/users.nix
+++ modules/users.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }:
+let
+  sshAuthorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC78zZLsyWn/h6q043x0hKK3VsK0As0YzJH61LX+wU11tttucWU0ES5i6O2qU4EpZAAfHR5H/vNvPVbupmG/MSi6eCJ/DOhpwoC4STt7y96P3Pw5daNijUNPcxrOLP1sp9CvAg9VEtvl74kIJEdn1HGUStyid4xOGafsJnE8C+SS+gshEW2zkhghg5Xoxm+s2PpPVjrUUWYBCUDZWa1Zb7pN9S/lPnHN72NcXOE12+o+fHHEPgBPovWEE0407jg8pOLH7JxuQIRQLxCmHEsN9Ikt9KG5WTtq70SDMoGnRCZJBj8qrF6ZFeUoFFDLj/olQqDKA9mpI1m9NPGHaCcGvRn openpgp:0xC98E1B0F"
+  ];
+in
 {
   programs.zsh.enable = true;
   programs.zsh.ohMyZsh.enable = true;
   users.mutableUsers = false;
   users.users = {
-    root.hashedPasswordFile = config.sops.secrets."users/password".path;
+    root = {
+      hashedPasswordFile = config.sops.secrets."users/password".path;
+      openssh.authorizedKeys = sshAuthorizedKeys;
+    };
     ccat3z = {
       isNormalUser = true;
       group = "users";
@@ -44,9 +52,7 @@
         moonlight-qt
       ];
       hashedPasswordFile = config.sops.secrets."users/password".path;
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC78zZLsyWn/h6q043x0hKK3VsK0As0YzJH61LX+wU11tttucWU0ES5i6O2qU4EpZAAfHR5H/vNvPVbupmG/MSi6eCJ/DOhpwoC4STt7y96P3Pw5daNijUNPcxrOLP1sp9CvAg9VEtvl74kIJEdn1HGUStyid4xOGafsJnE8C+SS+gshEW2zkhghg5Xoxm+s2PpPVjrUUWYBCUDZWa1Zb7pN9S/lPnHN72NcXOE12+o+fHHEPgBPovWEE0407jg8pOLH7JxuQIRQLxCmHEsN9Ikt9KG5WTtq70SDMoGnRCZJBj8qrF6ZFeUoFFDLj/olQqDKA9mpI1m9NPGHaCcGvRn openpgp:0xC98E1B0F"
-      ];
+      openssh.authorizedKeys = sshAuthorizedKeys;
       shell = pkgs.zsh;
     };
   };
