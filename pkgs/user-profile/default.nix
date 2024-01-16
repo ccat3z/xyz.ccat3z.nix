656179c8a3af5251353cc84dfe1788facc6fa842
ccat3z <fzhang.chn@outlook.com>
2024-01-16 17:13:53 +0800

    nix fmt

--- pkgs/user-profile/default.nix
+++ pkgs/user-profile/default.nix
@@ -1,9 +1,9 @@
 { lib, buildEnv, nix, nixos-rebuild, ... }:
 buildEnv {
-    name = "user-profile";
-    paths = [
-        nix
-        nixos-rebuild
-    ];
-    pathsToLink = ["/bin" "/etc" "/share"];
+  name = "user-profile";
+  paths = [
+    nix
+    nixos-rebuild
+  ];
+  pathsToLink = [ "/bin" "/etc" "/share" ];
 }
