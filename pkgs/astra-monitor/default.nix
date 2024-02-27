2f80297acde146b20eebc389dcd56ae252447db3
ccat3z <fzhang.chn@outlook.com>
2024-02-27 11:02:15 +0800

    nix fmt

--- pkgs/astra-monitor/default.nix
+++ pkgs/astra-monitor/default.nix
@@ -1,22 +1,22 @@
 { pkgs, fetchzip, gnomeExtensions, ... }:
 let
-    version = 12;
-    sha256 = "sMibsNSSZBjtfZqPnI+kF4cpRSvNZk4ni9qALS82/mU=";
-    inherit (gnomeExtensions) buildShellExtension;
+  version = 12;
+  sha256 = "sMibsNSSZBjtfZqPnI+kF4cpRSvNZk4ni9qALS82/mU=";
+  inherit (gnomeExtensions) buildShellExtension;
 in
 (buildShellExtension {
-    uuid = "monitor@astraext.github.io";
-    pname = "astra-monitor";
-    description = "Resource Monitor for GNOME shell";
-    link = "https://github.com/AstraExt/astra-monitor";
-    version = 12;
-    name = "";
-    inherit sha256;
-    metadata = "";
+  uuid = "monitor@astraext.github.io";
+  pname = "astra-monitor";
+  description = "Resource Monitor for GNOME shell";
+  link = "https://github.com/AstraExt/astra-monitor";
+  version = 12;
+  name = "";
+  inherit sha256;
+  metadata = "";
 }).overrideAttrs {
-    src = fetchzip {
-        url = "https://github.com/AstraExt/astra-monitor/releases/download/v${builtins.toString version}/monitor@astraext.github.io.shell-extension.zip";
-        stripRoot = false;
-        inherit sha256;
-    };
+  src = fetchzip {
+    url = "https://github.com/AstraExt/astra-monitor/releases/download/v${builtins.toString version}/monitor@astraext.github.io.shell-extension.zip";
+    stripRoot = false;
+    inherit sha256;
+  };
 }
--- pkgs/gnome/default.nix
+++ pkgs/gnome/default.nix
@@ -1,9 +1,9 @@
 { super, fetchpatch, ... }:
 super.gnome.overrideScope' (selfGnome: superGnome: {
-    mutter = superGnome.mutter.overrideAttrs {
-        patches = fetchpatch {
-            url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3525.patch";
-            hash = "sha256-/ubAWUqcyl+JfKgAt6dG3MFFlzqj1j7qcPpr+YHDhRA=";
-        };
+  mutter = superGnome.mutter.overrideAttrs {
+    patches = fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3525.patch";
+      hash = "sha256-/ubAWUqcyl+JfKgAt6dG3MFFlzqj1j7qcPpr+YHDhRA=";
     };
+  };
 })
