f8c3ed428b2d5abc3b16a615a4a23ff0363a3794
ccat3z <fzhang.chn@outlook.com>
2024-03-23 17:22:38 +0800

    nix fmt

--- hosts/nixos/pc-0/hardware.nix
+++ hosts/nixos/pc-0/hardware.nix
@@ -8,31 +8,41 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.initrd.availableKernelModules = [
-    "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod"
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "usb_storage"
+    "uas"
+    "sd_mod"
     # Required by luks key part
-    "vfat" "nls_cp437" "nls_iso8859_1"
+    "vfat"
+    "nls_cp437"
+    "nls_iso8859_1"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   boot.initrd.luks.devices = lib.mapAttrs
-    (n: v: let
-      keyMountPath = "/mnt/key";
-    in {
-      device = v;
-      keyFile = "${keyMountPath}/keys/${n}";
-      fallbackToPassword = true;
-      preOpenCommands = ''
-        echo "openning ${n}..."
+    (n: v:
+      let
+        keyMountPath = "/mnt/key";
+      in
+      {
+        device = v;
+        keyFile = "${keyMountPath}/keys/${n}";
+        fallbackToPassword = true;
+        preOpenCommands = ''
+          echo "openning ${n}..."
 
-        mkdir -p ${keyMountPath}
-        mount -t vfat ${config.fileSystems."/boot".device} ${keyMountPath}
-      '';
-      postOpenCommands = ''
-        umount ${keyMountPath}
-      '';
-    })
+          mkdir -p ${keyMountPath}
+          mount -t vfat ${config.fileSystems."/boot".device} ${keyMountPath}
+        '';
+        postOpenCommands = ''
+          umount ${keyMountPath}
+        '';
+      })
     {
       "root0" = "/dev/disk/by-uuid/056076b1-1099-4a42-b150-cf125ff2426e";
       "root1" = "/dev/disk/by-uuid/5ebb7404-2c04-4eb0-afde-552d0afb7c49";
