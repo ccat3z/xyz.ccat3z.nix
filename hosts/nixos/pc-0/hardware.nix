2817d7f37b48c926f484dae6eab9ef2b3cdaae27
ccat3z <fzhang.chn@outlook.com>
2024-03-23 13:05:10 +0800

    pc-0: Use key in boot part

--- hosts/nixos/pc-0/hardware.nix
+++ hosts/nixos/pc-0/hardware.nix
@@ -6,10 +6,36 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod"
+    # Required by luks key part
+    "vfat" "nls_cp437" "nls_iso8859_1"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  boot.initrd.luks.devices = lib.mapAttrs
+    (n: v: let
+      keyMountPath = "/mnt/key";
+    in {
+      device = v;
+      keyFile = "${keyMountPath}/keys/${n}";
+      fallbackToPassword = true;
+      preOpenCommands = ''
+        echo "openning ${n}..."
+
+        mkdir -p ${keyMountPath}
+        mount -t vfat ${config.fileSystems."/boot".device} ${keyMountPath}
+      '';
+      postOpenCommands = ''
+        umount ${keyMountPath}
+      '';
+    })
+    {
+      "root0" = "/dev/disk/by-uuid/056076b1-1099-4a42-b150-cf125ff2426e";
+      "root1" = "/dev/disk/by-uuid/5ebb7404-2c04-4eb0-afde-552d0afb7c49";
+    };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/562eab94-9110-469b-9ff6-21ac246a4748";
@@ -20,11 +46,6 @@
   fileSystems."/mnt/volume" = {
     device = "/dev/disk/by-uuid/562eab94-9110-469b-9ff6-21ac246a4748";
     fsType = "btrfs";
-  };
-
-  boot.initrd.luks.devices = {
-    "root0".device = "/dev/disk/by-uuid/056076b1-1099-4a42-b150-cf125ff2426e";
-    "root1".device = "/dev/disk/by-uuid/5ebb7404-2c04-4eb0-afde-552d0afb7c49";
   };
 
   fileSystems."/boot" = {
