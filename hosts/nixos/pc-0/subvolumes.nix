034e283f5371f9a1d0969ddde00c4e5eca2ed6ad
ccat3z <fzhang.chn@outlook.com>
2024-03-25 15:26:32 +0800

    pc-0: Create subvolume via tmpfile.d

--- hosts/nixos/pc-0/subvolumes.nix
+++ hosts/nixos/pc-0/subvolumes.nix
@@ -1,18 +1,13 @@
 { config, lib, ... }:
 let
-  device = "/dev/disk/by-uuid/562eab94-9110-469b-9ff6-21ac246a4748";
-
   inherit (config) myUser;
   myGroup = config.users.users.${myUser}.group;
 
-  subvolumes = {
+  device = "/dev/disk/by-uuid/562eab94-9110-469b-9ff6-21ac246a4748";
+  subvolOutOfRoot = {
     "/mnt/volume" = "/";
     "/home/${myUser}/Documents" = "project";
     "/home/${myUser}/Database" = "database";
-    "/home/${myUser}/.cache" = "nix/cache";
-    "/var/lib/syncthing" = "nix/syncthing";
-    "/var/lib/docker" = "nix/docker";
-    "/var/lib/libvirt" = "nix/libvirt";
   };
 
   mkFsCfg = path: subvol: {
@@ -25,10 +20,14 @@ let
   };
 in
 {
-  fileSystems = lib.listToAttrs (lib.mapAttrsToList mkFsCfg subvolumes);
+  fileSystems = lib.listToAttrs (lib.mapAttrsToList mkFsCfg subvolOutOfRoot);
 
+  # Subvol in rootfs
   systemd.tmpfiles.rules = [
-    "z /home/${myUser}/.cache 0755 ${myUser} ${myGroup}"
-    "z /var/lib/syncthing     0755 ${myUser} ${myGroup}"
+    "v /home/${myUser}/.cache    0755 ${myUser} ${myGroup}"
+    "v /home/${myUser}/Downloads 0755 ${myUser} ${myGroup}"
+    "v /var/lib/syncthing        0755 ${myUser} ${myGroup}"
+    "v /var/lib/docker           0700 root root"
+    "v /var/lib/libvirt          0700 root root"
   ];
 }
