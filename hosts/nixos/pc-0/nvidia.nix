6da65ac5c2be50b2e0b1fac04c033ed1d4518f99
ccat3z <fzhang.chn@outlook.com>
2024-04-10 16:00:09 +0800

    pc-0: Add a1111-webui-env

--- hosts/nixos/pc-0/nvidia.nix
+++ hosts/nixos/pc-0/nvidia.nix
@@ -4,6 +4,60 @@ let
   nvidia-modprobe = pkgs.nvidia-modprobe.override {
     version = nvidiaPackage.version;
   };
+
+  # Depends on https://github.com/virchau13/automatic1111-webui-nix
+  a1111-webui-env =
+    let
+      # Nvidia cuda deps
+      cudaPackages = pkgs.cudaPackages_12_1;
+      hardwareDeps = [
+        cudaPackages.cudatoolkit
+        nvidiaPackage
+      ] ++ (with pkgs; [
+        xorg.libXi
+        xorg.libXmu
+        freeglut
+        xorg.libXext
+        xorg.libX11
+        xorg.libXv
+        xorg.libXrandr
+        zlib
+
+        # for xformers
+        gcc
+      ]);
+
+      deps = with pkgs; [
+        git # The program instantly crashes if git is not present, even if everything is already downloaded
+        python310
+        stdenv.cc.cc.lib
+        stdenv.cc
+        ncurses5
+        binutils
+        gitRepo
+        gnupg
+        autoconf
+        curl
+        procps
+        gnumake
+        util-linux
+        m4
+        gperf
+        unzip
+        libGLU
+        libGL
+        glib
+      ];
+    in
+    pkgs.writeScriptBin "a1111-webui-env" ''
+      #! ${pkgs.runtimeShell}
+
+      export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (deps ++ hardwareDeps)}"
+      export CUDA_PATH="${cudaPackages.cudatoolkit}"
+      export EXTRA_LDFLAGS="-L${nvidiaPackage}/lib"
+
+      exec "$@"
+    '';
 in
 {
   boot.kernelModules = lib.mkBefore [ "nvidia" ]; # Make sure nvidia is load before vfio
@@ -18,4 +72,6 @@ in
         RUN+="${nvidia-modprobe}/bin/nvidia-modprobe -c 0", \
         RUN+="${nvidia-modprobe}/bin/nvidia-modprobe -c 0 -u"
     '';
+
+  my.home.packages = [ a1111-webui-env ];
 }
