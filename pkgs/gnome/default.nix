2b677d2161a6088a5521b7a1ed9e5d4a1979383f
ccat3z <fzhang.chn@outlook.com>
2024-05-18 15:02:30 +0800

    gnome: Fix clipboard

--- pkgs/gnome/default.nix
+++ pkgs/gnome/default.nix
@@ -1,9 +1,21 @@
 { super, fetchpatch, ... }:
 super.gnome.overrideScope' (selfGnome: superGnome: {
   mutter = superGnome.mutter.overrideAttrs {
+    # Fix clipboard
     patches = fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3525.patch";
-      hash = "sha256-/ubAWUqcyl+JfKgAt6dG3MFFlzqj1j7qcPpr+YHDhRA=";
+      url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3551.patch";
+      hash = "sha256-LO1fF/wup9YUOqsnJEq42PzIrMF4/rVAmtVgMiHqNz4=";
     };
+  };
+  gnome-shell = let
+    superGS = superGnome.gnome-shell;
+  in superGS.overrideAttrs {
+    patches = superGS.patches ++ [
+      # Required by mutter#3551
+      (fetchpatch {
+        url = "https://gitlab.gnome.org/tudmotu/gnome-shell/-/commit/2023121660fdfc3caa9baba5fd2ada877511dba3.patch";
+        hash = "sha256-zAnQXr7sLyKumH6WV4C4XSCuGbuMLRetiV/L1ok5BLU=";
+      })
+    ];
   };
 })
